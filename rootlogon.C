b95b1c9dd04d392529bf00aba7f160e6d2bc31ca
crafter312 <henrysquirewebb@gmail.com>
2025-07-29 13:25:46 -0500

    load dictionaries in rootlogon.C, added cos_thetaN output

--- rootlogon.C
+++ rootlogon.C
@@ -1,6 +1,10 @@
 {
+	// Load dictionary files
+	gSystem->Load("libLI6SIM.so");
+	TInterpreter::Instance()->AddIncludePath("libLI6SIM_rdict.pcm");
+
 	gStyle->SetPalette(kBird);
-	std::unique_ptr<TFile> myFile(TFile::Open("texneutsim-output_run0.root", "UPDATE"));
+	//std::unique_ptr<TFile> myFile(TFile::Open("texneutsim-output_run0.root", "UPDATE"));
 	TBrowser b;
 
 	gPad->SetTickx();
